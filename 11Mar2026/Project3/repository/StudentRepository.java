cf86e2c6cf8a0c28c5da3633a027b73f1e4344f3
ytdivyashreedivyashree-ops <ytdivyashreedivyashree@gmail.com>
2026-03-11 15:19:42 +0530

    Deleted Project3 folder

--- 11Mar2026/Project3/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Project3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Project3.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    
-}
